b920d59b1b293246b1bcd9d5f3a30a9df77aadb8
sonamgupta1010 <40820784+sonamgupta1010@users.noreply.github.com>
2018-07-26 21:36:11 +0530

    conversion of kilometer into miles

---
+++ exercise/kilometer.py
@@ -0,0 +1,8 @@
+kilometers = int(input("Enter value in kilometers"))
+
+
+conv_fac = 0.621371
+
+
+miles = kilometers * conv_fac
+print('%0.3f kilometers is equal to %0.3f miles' %(kilometers,miles))
