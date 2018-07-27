d7605c93ecad5f9b1dd3c62aa4461777add57bf5
sonamgupta1010 <40820784+sonamgupta1010@users.noreply.github.com>
2018-07-27 23:09:48 +0530

    pattern6

---
+++ exercise/patern6.py
@@ -0,0 +1,9 @@
+for i in range(1,5):
+    for j in range(1,9):
+        if j<=6-i or j>=4+i:
+            print("*",end=" ")
+        else:
+            print("*",end=" ")
+
+
+    print("\n")        
