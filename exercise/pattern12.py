ba3c22e0df319c865d17ea92178d4ac39ec9293c
sonamgupta1010 <40820784+sonamgupta1010@users.noreply.github.com>
2018-07-29 16:26:57 +0530

    pattern12

---
+++ exercise/pattern12.py
@@ -0,0 +1,11 @@
+for i in range(1,5):
+    for j in range(1,5):
+        if j<=i:
+            print(i,end="\t")
+        else:
+            print(" ",end="\t")
+
+
+
+    print("\n")        
+            
