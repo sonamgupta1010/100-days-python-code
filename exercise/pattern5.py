41c0b641f5a30c231b71e35411cf6b909561c822
sonamgupta1010 <40820784+sonamgupta1010@users.noreply.github.com>
2018-08-06 18:47:31 +0530

    updated exercise

--- exercise/pattern5.py
+++
@@ -1,8 +0,0 @@
-for i in range(1,5):
-    for j in range(1,9):
-        if j<=6-i or j>=4+i :
-            print("*",end="\t")
-            
-        else:
-            print("*",end="\t")
-            
