9ba142458df811a47e00998e2eb4d094e84d9476
sonamgupta1010 <40820784+sonamgupta1010@users.noreply.github.com>
2018-08-07 22:22:27 +0530

    sorting the list

---
+++ exercise/sort.py
@@ -0,0 +1,14 @@
+l=[]
+number=int(input("enter how many number="))
+print("enter number")
+for i in range(number):
+    data=int(input())
+    l.append(data)
+print (l)
+l.sort()
+print("after sorting")
+print (l)
+
+    
+    
+    
