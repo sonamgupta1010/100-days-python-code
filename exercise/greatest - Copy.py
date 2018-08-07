cb9fa0e703779b84aa60a4d2914af08c9d4c7b22
sonamgupta1010 <40820784+sonamgupta1010@users.noreply.github.com>
2018-08-07 22:23:19 +0530

    print greatest in list

---
+++ exercise/greatest - Copy.py
@@ -0,0 +1,9 @@
+l=[]
+number=int(input("enter how many number="))
+print("enter number")
+for i in range(number):
+    data=int(input())
+    l.append(data)
+print(l)
+print("greatest number is=")
+print(max(l))
