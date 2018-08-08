ce3b53b49ded0979842ff3bb736474ef9e4df6a8
sonamgupta1010 <40820784+sonamgupta1010@users.noreply.github.com>
2018-08-08 18:05:07 +0530

    print lsum of all natural number in list

---
+++ exercise/day36/sumlist.py
@@ -0,0 +1,8 @@
+l=[]
+number=int(input("enter how many number="))
+print("enter the number=")
+for i in range(number):
+    data=int(input())
+    l.append(data)
+print(l)
+print("sum of int in list is",sum(l))
