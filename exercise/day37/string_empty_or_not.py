48c325e985edbbc06c284cc4c381c4f2cde72521
sonamgupta1010 <40820784+sonamgupta1010@users.noreply.github.com>
2018-08-09 21:38:28 +0530

    check string empty or not

---
+++ exercise/day37/string_empty_or_not.py
@@ -0,0 +1,17 @@
+# check a string is empty or not
+fname="joginder"
+lname=""
+if fname:
+    print("fname String is not empty")
+else:
+    print("fname String is empty") 
+if lname:
+    print("lname String is not empty")
+else:
+    print("lname String is empty")       
+
+
+# output:
+
+# fname String is not empty
+# lname String is empty
