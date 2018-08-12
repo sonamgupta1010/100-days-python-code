006b2e37f9a0649b55a149940b88d6bdbb97bf70
sonamgupta1010 <40820784+sonamgupta1010@users.noreply.github.com>
2018-08-12 21:44:01 +0530

    conversion of decimal into binary ,octal and hexadecimal

---
+++ exercise/day39/conversion.py
@@ -0,0 +1,8 @@
+# Change this line for a different result
+dec = int(input("enter any decimal number: "))
+
+print("The decimal value of",dec,"is:")
+print(bin(dec),"in binary.")
+print(oct(dec),"in octal.")
+print(hex(dec),"in hexadecimal.")
+ 
