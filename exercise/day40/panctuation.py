bab706461ffce97bc7e1a57ecd1e8109fc552cd8
sonamgupta1010 <40820784+sonamgupta1010@users.noreply.github.com>
2018-08-13 21:49:21 +0530

    remove panctuation from string

---
+++ exercise/day40/panctuation.py
@@ -0,0 +1,14 @@
+
+punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+
+my_str = input("Enter a string: ")
+
+
+no_punct = ""
+for char in my_str:
+   if char not in punctuations:
+       no_punct = no_punct + char
+
+# display the unpunctuated string
+print(no_punct)
