fc6e2454681e2c652debd713630308d6e664b470
sonamgupta1010 <40820784+sonamgupta1010@users.noreply.github.com>
2018-08-16 23:55:19 +0530

    get ip address

---
+++ exercise/day43/ip.py
@@ -0,0 +1,8 @@
+import socket;
+print("Want to get IP Address ? (y/n): ");
+check = input();
+if check == 'n':
+    exit();
+else:
+    print("\nYour IP Address is: ",end="");
+    print(socket.gethostbyname(socket.gethostname()));
