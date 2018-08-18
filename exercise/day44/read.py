9fdf8534c4c2615ced5641c1dad8a5e2815200c0
sonamgupta1010 <40820784+sonamgupta1010@users.noreply.github.com>
2018-08-18 22:59:50 +0530

    read a file

---
+++ exercise/day44/read.py
@@ -0,0 +1,10 @@
+print("Enter 'x' for exit.");
+filename = input("Enter file name (with extension) to read: ");
+if filename == 'x':
+    exit();
+else:
+    c = open(filename, "r");
+    print("\nThe file,",filename,"opened successfully!");
+    print("The file",filename,"contains:\n");
+    print(c.read())
+    c.close()
