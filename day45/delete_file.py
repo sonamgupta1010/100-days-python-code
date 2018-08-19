a663e118c128447c058c494250cc191f80a91d46
sonamgupta1010 <40820784+sonamgupta1010@users.noreply.github.com>
2018-08-19 23:12:47 +0530

    delete choosen file

---
+++ day45/delete_file.py
@@ -0,0 +1,9 @@
+import os;
+print("Enter 'x' for exit.");
+filename = input("Enter name of file to delete: ");
+if filename == 'x':
+    exit();
+else:
+    print("\nRemoving the file....");
+    os.remove(filename);
+    print("\nFile,",filename,"successfully deleted!!");
