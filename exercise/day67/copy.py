382eab88db65d497fecd0979de3d97bb33c78fe2
sonamgupta1010 <40820784+sonamgupta1010@users.noreply.github.com>
2018-09-10 23:46:52 +0530

    print all number present in text file

---
+++ exercise/day67/copy.py
@@ -0,0 +1,9 @@
+fname = input("Enter file name: ")
+ 
+with open(fname, 'r') as f:
+    for line in f:
+        words = line.split()
+        for i in words:
+            for letter in i:
+                if(letter.isdigit()):
+                    print(letter)
