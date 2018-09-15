6646167e3a04aca0f4525b4a3eba7fd933b285ac
sonamgupta1010 <40820784+sonamgupta1010@users.noreply.github.com>
2018-09-15 23:52:34 +0530

    append content of one file into another

---
+++ exercise/day70/append_content.py
@@ -0,0 +1,8 @@
+name1 = input("Enter file to be read from: ")
+name2 = input("Enter file to be appended to: ")
+fin = open(name1, "r")
+data2 = fin.read()
+fin.close()
+fout = open(name2, "a")
+fout.write(data2)
+fout.close()
