18f0be3687597b25ffa99b490e2b39f2de1aea67
sonamgupta1010 <40820784+sonamgupta1010@users.noreply.github.com>
2018-09-16 14:17:09 +0530

    count word in text file

---
+++ exercise/day71/words.py
@@ -0,0 +1,10 @@
+fname = input("Enter file name: ")
+ 
+num_words = 0
+ 
+with open(fname, 'r') as f:
+    for line in f:
+        words = line.split()
+        num_words += len(words)
+print("Number of words:")
+print(num_words)
