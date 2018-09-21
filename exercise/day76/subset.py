434f18924e8f63516f7af77b7bd7da14eac11cc6
sonamgupta1010 <40820784+sonamgupta1010@users.noreply.github.com>
2018-09-21 23:19:36 +0530

    create class by set geeting all subset

---
+++ exercise/day76/subset.py
@@ -0,0 +1,15 @@
+class sub:  
+    def f1(self, s1):  
+        return self.f2([], sorted(s1))  
+ 
+    def f2(self, curr, s1):  
+        if s1:  
+            return self.f2(curr, s1[1:]) + self.f2(curr + [s1[0]], s1[1:])  
+        return [curr]  
+a=[]
+n=int(input("Enter number of elements of list: "))
+for i in range(0,n):
+    b=int(input("Enter element: "))
+    a.append(b)
+print("Subsets: ")
+print(sub().f1(a))
